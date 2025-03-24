6536caef4460f4fb09403dc270e652df7b620b23
Rhys Wells <rhyslwells@outlook.com>
2025-03-24 20:34:27 +0000

    sqlite reformat

--- Explorations/SQLite/Writing/d_delete/delete2.sql
+++
@@ -1,5 +0,0 @@
--- Demonstrates deleting rows with ON DELETE actions
--- Uses mfa.db
-
--- Deletes an artist when foreign key ON DELETE action is set to CASCADE
-DELETE FROM "artists" WHERE "name" = 'Unidentified artist';
--- Explorations/SQLite/Writing/d_insert/import/import1/import1.sql
+++
@@ -1,8 +0,0 @@
--- Import into an existing table using a CSV without primary keys
-
-.import --csv mfa.csv temp
-
-INSERT INTO "collections" ("title", "accession_number", "acquired") 
-SELECT "title", "accession_number", "acquired" FROM "temp";
-
-DROP TABLE "temp";
